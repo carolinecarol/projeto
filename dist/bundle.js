bc182835e486e0fc1a4640b65c6d90d491ccb485
Juliana Planck <juliana.vaz@cps.sp.gov.br>
2018-10-28 00:46:37 -0300

    inserindo carousel na respectiva section

--- dist/bundle.js
+++ dist/bundle.js
@@ -6,3 +6,59 @@ botaoMenu.onclick = () => {
   escondeMenu.classList.toggle('visible');
   }
   
+let carousels = document.getElementsByClassName('img-carousel');
+
+[].forEach.call(carousels, function(c) {
+  let next = c.getElementsByClassName('next')[0],
+    prev = c.getElementsByClassName('prev')[0],
+    bubblesContainer = c.getElementsByClassName('bubble')[0],
+    inner = c.getElementsByClassName('inner'),
+    imgs = inner.getElementsByTagName('img'),
+    currentImageIndex = 0,
+    width = 640,
+    bubbles = [],
+
+
+    for (let i = 0; i < imgs.lenght; i++) {
+      let b = document.createElement('span');
+      b.classList.add('bubbles');
+      bubblesContainer.appenChild(b);
+      bubbles.push(b);
+
+      b.addEventListener('click', function () {
+        currentImageIndex = i;
+        switchImg();
+      });
+    }
+
+    function switchImg(){
+      inner.Style.Left = -width * currentImageIndex + 'px';
+    }
+
+      bubbles.forEach(function (b, i) {
+        if (i === currentImageIndex) {
+          b.classList.add('active');
+        } else {
+          b.classList.remove('active');
+        }
+      });
+    
+
+    next.addEventListener('click', function(){
+      currentImageIndex++;
+        if (currentImageIndex >= imgs.lenght) {
+             currentImageIndex = 0;
+                  }
+        switchImg();
+                });
+    
+    prev.addEventListener('click', function(){
+      currentImageIndex--;
+         if (currentImageIndex < 0) {
+              currentImageIndex = imgs.lenght - 1;
+              }
+              switchImg();
+            });
+
+            switchImg();
+          });
